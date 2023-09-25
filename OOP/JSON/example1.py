684cd372d996f8dc13b6afa684b2abb29f3bbf29
DAVENJAGI <njagidave39@gmail.com>
2023-09-25 08:22:58 -0400

    JSON

--- OOP/JSON/example1.py
+++ OOP/JSON/example1.py
@@ -2,9 +2,7 @@ import json
 
 with open('states.json') as f:
     data = json.load(f)
+print(data)
 
-for state in data['states']:
-    print(state)
-if __name__ == "__main__":
-    json_file_name = "states.json"
-    run_json_file(states.json)
+#for state in data['states']:
+#    print(state)
