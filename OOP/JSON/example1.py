7387a17b7b74df83fbafb413f06b9d20f7ff4d4c
DAVENJAGI <njagidave39@gmail.com>
2023-10-07 17:54:48 -0400

    JSON

--- OOP/JSON/example1.py
+++ OOP/JSON/example1.py
@@ -1,9 +1,17 @@
-#!/usr/bin/python3
 import json
 
-with open('states.json') as f:
-    data = json.load(f)
-print(data)
+with open("states.json") as f:
+    data =  json.load(f)
+#print(data)
 
 #for state in data['states']:
 #    print(state)
+
+for state in data['states']:
+    del state['area_codes']
+#    print(data)
+
+with open('new_states.json', 'w') as j:
+          json.dump(data, j, indent=2)
+#    print(state['name'], ':', state['abbreviation'])
+
